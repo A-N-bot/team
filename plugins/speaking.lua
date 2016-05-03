57f5fdee9bc6a290ea9cfe07652d36b2558d05eb
anonymou3tea <anonymou3team@gmail.com>
2016-05-03 21:09:16 +0430

    Update speaking.lua

--- plugins/speaking.lua
+++ plugins/speaking.lua
@@ -1,81 +1,81 @@
 local function run(msg)
 if msg.text == "hi" then
-	return "های"
+	return "👋های"
 end
 if msg.text == "Hi" then
-	return "سلام عزیزم"
+	return "😉سلام عزیزم"
 end
 if msg.text == "Hello" then
-	return "سلام"
+	return "✋سلام"
 end
 if msg.text == "hello" then
-	return "سلوم عجقم"
+	return "💔سلوم عجقم"
 end
 if msg.text == "Salam" then
-	return "سلام علیکم"
+	return "😌سلام علیکم"
 end
 if msg.text == "salam" then
-	return "علیکم السلام"
+	return "😂علیکم السلام"
 end
 if msg.text == "سلام" then
-	return "Hi"
+	return "Hi😃"
 end
 if msg.text == "س" then
-	return "مثل آدم بنویس سلام!"
+	return "😏مثل آدم بنویس سلام!"
 end
 if msg.text == "kir" then
-	return "to konet"
+	return "to konet😲"
 end
 if msg.text == "کیر" then
-	return "تو کونت"
+	return "تو کونت😄"
 end
 if msg.text == "fuck" then
-	return "you ! _|_"
+	return "😡you ! _|_"
 end
-if msg.text == "tiago" then
-	return "جانم؟"
+if msg.text == "anonymous" then
+	return "جانم؟😁"
 end
-if msg.text == "Tiago" then
-	return "جانم؟"
+if msg.text == "Anonymous" then
+	return "جانم؟😍"
 end
-if msg.text == "jan123" then
+if msg.text == "Nima" then
+	return "با بابام کاری داری؟ 😎 تو @anonymousPv_bot بهم بگو بهش میگم 🤗"
+end
+if msg.text == "nima" then
+	return "با بابام کاری داری؟ 😎 تو @anonymousPv_bot بهم بگو بهش میگم 🤗"
+end
+if msg.text == "نیما" then
 	return "با بابام کاری داری؟ 😎 تو @TiagoPvbot بهم بگو بهش میگم 🤗"
 end
-if msg.text == "Jan123" then
-	return "با بابام کاری داری؟ 😎 تو @TiagoPvbot بهم بگو بهش میگم 🤗"
-end
-if msg.text == "جان123" then
-	return "با بابام کاری داری؟ 😎 تو @TiagoPvbot بهم بگو بهش میگم 🤗"
-end
-if msg.text == "تیاگو" then
-	return "کاری داشتی؟"
+if msg.text == "انونیموس" then
+	return "کاری داشتی؟😜"
 end
 if msg.text == "bot" then
-	return "من ربات نیستم !"
+	return "😒من ربات نیستم !"
 end
 if msg.text == "ربات" then
-	return "من ربات نیستم !"
+	return "😒من ربات نیستم !"
 end
 if msg.text == "بات" then
-	return "من ربات نیستم !"
+	return "😒من ربات نیستم !"
 end
 if msg.text == "روبات" then
-	return "من ربات نیستم !"
+	return "😒من ربات نیستم !"
 end
 if msg.text == "Bot" then
-	return "Huuuum?"
+	return "Huuuum?😉"
 end
 if msg.text == "Bye" then
-	return "بای عجیجم"
+	return "😃بای عجیجم"
 end
 if msg.text == "bye" then
-	return "خدا حافظ"
+	return "😞خدا حافظ"
 end
 if msg.text == "بای" then
-	return "Bb"
+	return "Bb✋"
 end
 if msg.text == "خداحافظ" then
-	return "Bye"
+	return "Bye👋"
 end
 end
 
@@ -94,10 +94,10 @@ return {
 		"^?$",
 		"^[kK][iI][rR]$",
 		"^کیر$",
-		"^تیاگو$",
-		"^[Tt]iago$",
-		"^[Jj]an123$",
-		"^جان123$",
+		"^انونیموس$",
+		"^[Aa]nonymous$",
+		"^[Nn]ima$",
+		"^نیما$",
 		"^[Ss]alam$",
 		"^بات$",
 		"^ربات$",
